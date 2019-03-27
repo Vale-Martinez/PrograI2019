2dabbf134fdfb7b3784e90d1873be6da36b53c9d
Vale-Martinez <42896468+Vale-Martinez@users.noreply.github.com>
2019-03-27 09:26:11 -0600

    Add files via upload

---
+++ mavenproject1/src/main/java/logica/tiquetes/LoteriaTiquete.java
@@ -0,0 +1,30 @@
+package logica.tiquetes;
+import logica.gestores.Tiquetes;
+
+/**
+ *
+ * @author valem
+ */
+
+public class LoteriaTiquete extends Tiquetes{
+
+    void crear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void eliminar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void modificar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Object consultar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+
+    
+    
+}
